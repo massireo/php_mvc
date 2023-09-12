d4afd13ae5f70d35e558669f072e566789964517
massireo <144077634+massireo@users.noreply.github.com>
2023-09-12 09:23:13 +0700

    Add files via upload

---
+++ phpmvc/phpmvc.sql
@@ -0,0 +1,52 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 12, 2023 at 04:22 AM
+-- Server version: 10.4.24-MariaDB
+-- PHP Version: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `phpmvc`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `data_siswa`
+--
+
+CREATE TABLE `data_siswa` (
+  `id` int(10) NOT NULL,
+  `nama` varchar(255) NOT NULL,
+  `jenis_kelamin` varchar(255) NOT NULL,
+  `alamat` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `data_siswa`
+--
+
+INSERT INTO `data_siswa` (`id`, `nama`, `jenis_kelamin`, `alamat`) VALUES
+(0, 'zidan', 'laki-laki', 'kampung lima'),
+(0, 'evta', 'laki-laki', 'kampung lima'),
+(0, 'vektor', 'laki-laki', 'kampung lima'),
+(0, 'saya', 'laki-laki', 'kampung lima'),
+(0, 'orang baru', 'laki-laki', 'kampung lima'),
+(0, 'orang lawas', 'laki-laki', 'kampung lima');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
